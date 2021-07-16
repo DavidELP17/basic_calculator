bf9a617b71ac19d1507490807b624d2c9489f639
David Londono <davidelp94@gmail.com>
2021-07-16 13:36:52 -0500

    Basic Operations Completed

--- index.js
+++ index.js
@@ -3,7 +3,39 @@ const app = express()
 const PORT = 8080
 
 app.get('/api/sum', (req,res) => {
-    res.send('Hello world')
+    let op1 = Number(req.query.n1)
+    let op2 = Number(req.query.n2)
+
+    result = op1 + op2
+
+    res.json(`result = ${result}`)
+})
+
+app.get('/api/sub', (req,res) => {
+    let op1 = Number(req.query.n1)
+    let op2 = Number(req.query.n2)
+
+    result = op1 - op2
+
+    res.json(`result = ${result}`)
+})
+
+app.get('/api/mul', (req,res) => {
+    let op1 = Number(req.query.n1)
+    let op2 = Number(req.query.n2)
+
+    result = op1 * op2
+
+    res.json(`result = ${result}`)
+})
+
+app.get('/api/div', (req,res) => {
+    let op1 = Number(req.query.n1)
+    let op2 = Number(req.query.n2)
+
+    result = op1 / op2
+
+    res.json(`result = ${result}`)
 })
 
 app.listen(PORT,()=>{
